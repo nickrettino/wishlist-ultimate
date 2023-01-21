f6ff31080dbcd150a2e1d7e634d399bb6a048e14
nickrettino <88734682+nickrettino@users.noreply.github.com>
2023-01-21 16:54:39 -0500

    website running with bootstrap
    website up and running through bootstrap

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 """New website"""
-
-import flask
-import flask_bootstrap
 import flask_sqlalchemy
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
-app = Flask(__name__)
+
+def create_app():
+    """use Bootsrap"""
+    app = Flask(__name__)
+    Bootstrap(app)
+
+    @app.route("/")
+    def home_page():
+        """this is the route for the homepage of the website"""
+
+        return render_template("index.html")
+
+    return app
 
 
-@app.route("/")
-def home_page():
-    return "<p> Hello, World!</p>"
+app = create_app()
+
+if __name__ == "__main__":
+    app.run(debug=True)
