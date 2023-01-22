78f75b7ed04c158c336b52ff0beba668363e6f10
nickrettino <88734682+nickrettino@users.noreply.github.com>
2023-01-22 17:06:46 -0500

    added button to login page centered on image

--- main.py
+++ main.py
@@ -2,6 +2,9 @@
 import flask_sqlalchemy
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired
 
 
 def create_app():
@@ -17,6 +20,11 @@ frontend = create_app()
 @frontend.route("/")
 def homepage():
     return render_template("index.html")
+
+
+@frontend.route("/login")
+def login_page():
+    return render_template("login.html")
 
 
 if __name__ == "__main__":
