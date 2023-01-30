9308bbce22a34a6147b105c0435a763190ca6447
nickrettino <88734682+nickrettino@users.noreply.github.com>
2023-01-30 14:31:13 -0500

    Form centered and placeholders made

--- forms.py
+++ forms.py
@@ -1,11 +1,11 @@
 """script for all form classes"""
 
 from flask_wtf import FlaskForm
-from wtforms.fields import EmailField, PasswordField, StringField
+from wtforms.fields import EmailField, PasswordField
 from wtforms.validators import DataRequired
 
 
 class LoginForm(FlaskForm):
 
-    email = EmailField("email", validators=[DataRequired()])
-    password = PasswordField("password", validators=[DataRequired()])
+    email = EmailField("Email", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
